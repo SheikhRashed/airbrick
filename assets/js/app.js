2ef5512bb6f642fbc1e90c7cb813365345c3848a
SheikhRashed <devrashed06@gmail.com>
2022-03-12 06:16:11 +0600

    update: range slider added

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,49 @@
 // Import
 
 $(".count").countUp()
+
+var inAmount = document.getElementById("inamount")
+var inLength = document.getElementById("inLength")
+var inPercent = document.getElementById("inPercent")
+let inAmountOutput = document.querySelector(".output span")
+let inLengthOutput = document.querySelector(".output-length span")
+let inPercentOutput = document.querySelector(".output-percent span")
+
+noUiSlider.create(inAmount, {
+	start: [0, 0],
+	connect: true,
+	range: {
+		min: 0,
+		max: 50000,
+	},
+})
+
+noUiSlider.create(inLength, {
+	start: [0, 0],
+	connect: true,
+	range: {
+		min: 1,
+		max: 60,
+	},
+})
+
+noUiSlider.create(inPercent, {
+	start: [0, 0],
+	connect: true,
+	range: {
+		min: 1,
+		max: 100,
+	},
+})
+
+inAmount.noUiSlider.on("update", function (values, handle) {
+	inAmountOutput.innerHTML = Math.floor(values[handle])
+})
+
+inLength.noUiSlider.on("update", function (values, handle) {
+	inLengthOutput.innerHTML = Math.floor(values[handle])
+})
+
+inPercent.noUiSlider.on("update", function (values, handle) {
+	inPercentOutput.innerHTML = Math.floor(values[handle])
+})
